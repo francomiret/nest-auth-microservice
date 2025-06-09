480b3cbe424008860e4f2968e3a32063efc9217a
Franco Miret <francomiret11@gmail.com>
2025-06-09 00:02:31 -0300

    feat: add jwtModule configuration register in auth microservice

--- nest-auth-microservice/apps/auth-microservice/src/auth/auth.module.ts
+++ nest-auth-microservice/apps/auth-microservice/src/auth/auth.module.ts
@@ -1,8 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { JwtModule } from '@nestjs/jwt';
+import { config } from '../config';
 
 @Module({
+  imports: [
+    JwtModule.register({
+      global: true,
+      secret: config.jwtSecret,
+      signOptions: { expiresIn: '1h' },
+    }),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
--- nest-auth-microservice/apps/auth-microservice/src/auth/auth.service.ts
+++ nest-auth-microservice/apps/auth-microservice/src/auth/auth.service.ts
@@ -1,9 +1,15 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { RegisterUserDto } from './dto/register-user.dto';
 import { LoginUserDto } from './dto/login-user.dto';
+import { JwtPayload } from './interfaces/jwt.payload.interface';
+import { JwtService } from '@nestjs/jwt';
+import { config } from '../config';
+import { RpcException } from '@nestjs/microservices';
 
 @Injectable()
 export class AuthService {
+  constructor(private readonly jwtService: JwtService) {}
+
   async register(registerDto: RegisterUserDto) {
     return registerDto;
   }
@@ -13,6 +19,25 @@ export class AuthService {
   }
 
   async verifyToken(token: string) {
-    return token;
+    try {
+      const {
+        sub: _,
+        iat: __,
+        exp: ___,
+        ...user
+      } = this.jwtService.verify(token, {
+        secret: config.jwtSecret,
+      });
+      return { user, token: this.signJwt(user) };
+    } catch (error) {
+      throw new RpcException({
+        code: 400,
+        message: error.message,
+      });
+    }
+  }
+
+  signJwt(payload: JwtPayload) {
+    return this.jwtService.sign(payload);
   }
 }
