0d480d25c8bfca219c126d8cdb088c2dd70d70df
Franco Miret <francomiret11@gmail.com>
2025-06-20 20:53:31 -0300

    refactro: update main.ts in members microservice

--- nest-auth-microservice/apps/members-microservice/src/main.ts
+++ nest-auth-microservice/apps/members-microservice/src/main.ts
@@ -3,19 +3,31 @@
  * This is only a minimal backend to get started.
  */
 
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
+import { config } from './config';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3000;
-  await app.listen(port);
-  Logger.log(
-    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
+  const logger = new Logger('Members Microservice');
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.NATS,
+      options: {
+        servers: config.natsServer,
+      },
+    }
   );
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    })
+  );
+  await app.listen();
+  logger.log(`🚀 Application is running on: ${config.natsServer}`);
 }
 
 bootstrap();
