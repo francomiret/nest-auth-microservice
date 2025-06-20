0200756964798c44738a6ff266d4dd6b9284bb68
Franco Miret <francomiret11@gmail.com>
2025-06-20 18:43:28 -0300

    feat: add register configuration

--- nest-auth-microservice/apps/auth-microservice/src/auth/auth.service.ts
+++ nest-auth-microservice/apps/auth-microservice/src/auth/auth.service.ts
@@ -5,13 +5,40 @@ import { JwtPayload } from './interfaces/jwt.payload.interface';
 import { JwtService } from '@nestjs/jwt';
 import { config } from '../config';
 import { RpcException } from '@nestjs/microservices';
+import { PrismaClient } from '@prisma/client';
+import { OnModuleInit } from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
-export class AuthService {
-  constructor(private readonly jwtService: JwtService) {}
+export class AuthService extends PrismaClient implements OnModuleInit {
+  constructor(private readonly jwtService: JwtService) {
+    super();
+  }
+
+  async onModuleInit() {
+    await this.$connect();
+  }
 
   async register(registerDto: RegisterUserDto) {
-    return registerDto;
+    const exist = await this.user.findUnique({
+      where: {
+        email: registerDto.email,
+      },
+    });
+    if (exist) {
+      throw new RpcException({
+        code: 400,
+        message: 'User already exists',
+      });
+    }
+    const hashedPassword = await bcrypt.hashSync(registerDto.password, 10);
+    const user = await this.user.create({
+      data: {
+        ...registerDto,
+        password: hashedPassword,
+      },
+    });
+    return user;
   }
 
   async login(loginDto: LoginUserDto) {
